b39e9da5f3518eba2a6a68243bc1617132df329f
doquockhanh <khanhquocdo.qt@gmail.com>
2024-05-24 14:32:26 +0700

    lab 7

---
+++ lab7/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bai1();
+    }
+
+    static void bai1()
+    {
+        Console.WriteLine("Bai 1: ");
+
+        Console.WriteLine("A)");
+        List<int> nums = new List<int>() { 1, 3, 4, 5, 8, 9 };
+        string a = string.Join(", ", nums.Where(x => x > 4).Select(x => x * x));
+        Console.WriteLine($"Binh phuong cac so lon hon 4 la: \n{a}");
+
+        Console.WriteLine("B)");
+        string str = "chao mung den voi binh nguyen vo tan";
+        var characterCount = str
+                             .Where(c => !char.IsWhiteSpace(c))
+                             .GroupBy(c => c)
+                             .Select(g => new { Character = g.Key, Count = g.Count() })
+                             .ToList();
+        Console.WriteLine("Số lần xuất hiện của từng chữ:");
+        foreach (var item in characterCount)
+        {
+            Console.WriteLine($"'{item.Character}': {item.Count}");
+        }
+
+        Console.WriteLine("C)");
+        string str2 = "CHAO mung DEN Voi binh nguyen vo tan"; 
+        var result = str2
+                        .Split(" ")                           
+                        .Where(word => word.Equals(word.ToUpper()))
+                        .ToList();
+        Console.WriteLine("Cac chu viet hoa toan bo:");
+        Console.WriteLine(string.Join(", ", result));     
+    }
+}
